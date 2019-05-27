77cb42a8fa05f228b66c63777ed47eb2c3aef4c3
kate dubitski <katedubitski@ip-192-168-0-21.ec2.internal>
2019-05-27 19:46:24 -0400

    added example of number and boolean types

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 //string
 var myName = "kate";
-myName = 34;
+/*
+myName = 34; //Error, myName is set up as a type of string
+*/
+//numbers
+var favNum = 8;
+/*
+favNum = "3"; //ERror, favNum is expecting a number!
+*/
+//boolean
+var hasHobbies = true;
+hasHobbies = 1;
--- app.ts
+++ app.ts
@@ -3,3 +3,15 @@ let myName = "kate";
 /*
 myName = 34; //Error, myName is set up as a type of string
 */
+
+//numbers
+let favNum = 8;
+/*
+favNum = "3"; //ERror, favNum is expecting a number!
+*/
+
+//boolean
+let hasHobbies = true;
+/*
+hasHobbies = 1; //Error, 1 is not a boolean!!!!
+*/
