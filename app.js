16378f8c638a192fc0383b71204287708fb72ed9
kate dubitski <katedubitski@ip-192-168-0-21.ec2.internal>
2019-05-27 19:55:52 -0400

    added implicit types

--- app.js
+++ app.js
@@ -10,4 +10,15 @@ favNum = "3"; //ERror, favNum is expecting a number!
 */
 //boolean
 var hasHobbies = true;
-hasHobbies = 1;
+/*
+hasHobbies = 1; //Error, 1 is not a boolean!!!!
+*/
+//assign types
+var myRealAge;
+myRealAge = 16;
+myRealAge = "16";
+//this works becouse we did not specify what kind of
+//type the myRealAge variable is expecting so it was setup to type any.
+var myCatAge;
+myCatAge = 13;
+//myCatAge = "13"; => this is wrong becouse its expecting type number
--- app.ts
+++ app.ts
@@ -1,17 +1,29 @@
 //string
-let myName = "kate";
+let myName: string = "kate";
 /*
 myName = 34; //Error, myName is set up as a type of string
 */
 
 //numbers
-let favNum = 8;
+let favNum: number = 8;
 /*
 favNum = "3"; //ERror, favNum is expecting a number!
 */
 
 //boolean
-let hasHobbies = true;
+let hasHobbies: boolean = true;
 /*
 hasHobbies = 1; //Error, 1 is not a boolean!!!!
 */
+
+//assign types
+
+let myRealAge;
+myRealAge = 16;
+myRealAge = "16";
+//this works becouse we did not specify what kind of
+//type the myRealAge variable is expecting so it was setup to type any.
+
+let myCatAge: number;
+myCatAge = 13;
+//myCatAge = "13"; => this is wrong becouse its expecting type number
